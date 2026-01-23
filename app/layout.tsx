c386d6c05a46ce6fca2bdb7ba3c67699819bea70
Faheem Mansuri <mansurifaheem1111@google.com>
2026-01-23 11:23:45 +0530

    Fix Issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-import localFont from "next/font/local";
-import { Toaster } from "@/components/ui/sonner"
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
-const geist = localFont({
-  src: "./../public/fonts/Geist-Regular.woff2",
-  variable: "--font-geist",
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
-const geistMono = localFont({
-  src: "./../public/fonts/GeistMono-Regular.woff2",
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Signalist",
-  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
+  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights",
 };
 
 export default function RootLayout({
@@ -27,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ fontFamily: "system-ui, sans-serif" }}
       >
         {children}
         <Toaster />
